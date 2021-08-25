a9bab7e9eef31186d96272453251829a4561dfa5
INSANE BOY <87981808+insanebwoi@users.noreply.github.com>
2021-08-25 15:02:36 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -130,7 +130,7 @@ const array = ['add','back','johny','kundan','njangal','podi','sad','tagall','va
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
-       await message.client.sendMessage(message.jid, fs.readFileSync('./media/uploads/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
+       await message.client.sendMessage(message.jid, fs.readFileSync('./media/uploads/' + a + '.mp3'), MessageType.audio, {contextInfo: { forwardingScore: 10, isForwarded: true }, mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
 }
 });
     }
@@ -163,7 +163,7 @@ const array = ['Hi','Fek','Ariyo','Ayn','Aysheri','Ayye','Baby','Bot','Chill','D
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
-       await message.client.sendMessage(message.jid, fs.readFileSync('./media/files/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
+       await message.client.sendMessage(message.jid, fs.readFileSync('./media/files/' + a + '.mp3'), MessageType.audio,  mimetype:{contextInfo: { forwardingScore: 10, isForwarded: true }, Mimetype.mp4Audio, quoted: message.data, ptt: true})
 }
 });
     }
