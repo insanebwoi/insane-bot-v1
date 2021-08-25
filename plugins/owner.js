af685dbd675d9bb697808c6ca71d750d131e8f2e
SafwanGanz <67726326+SafwanGanz@users.noreply.github.com>
2021-08-25 19:02:35 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,3 +1,13 @@
-\\by safwan ganz :v
-\\ don't copy paste this script 
-\\©SafwanGanz
+
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + 'FN: INSANE BOY\n' 
+            + 'ORG:OWNER INSANE BOT;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=917012074386:+917012074386\n' 
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid, {displayname: "INSANE BOY", vcard: vcard}, MessageType.contact, {quoted: mek})
+Asena.addCommand({pattern: 'owner', fromMe: false, desc: `hehe`}, (async (message, match) => {
+}));
+
